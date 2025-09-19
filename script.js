bf0c8b48a7405b074be8b8895876d21355aec217
John Paini <johnpaini@gmail.com>
2025-09-19 00:40:08 -0400

    atualizações de layout

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+
 document.querySelectorAll('nav a').forEach(link => {
   link.addEventListener('click', e => {
     e.preventDefault();
     const target = document.querySelector(link.getAttribute('href'));
     target.scrollIntoView({ behavior: 'smooth' });
   });
+});
+
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
 });
